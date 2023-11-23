f7ac647ad9e127763e5cf6d70bdfb1a60e70a7d6
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-11-23 00:03:25 -0500

    basic patching of single source file with single dep

--- lib/mkRocDerivation.nix
+++ lib/mkRocDerivation.nix
@@ -1,4 +1,4 @@
-{ stdenv, downloadMultipleRocPackages, roc }:
+{ jq, stdenv, downloadMultipleRocPackages, roc }:
 
 { name ? "${args.pname}-${args.version}", src ? null
 , rocDeps # list of {url,sha256} for all external deps of the roc program
@@ -9,12 +9,14 @@ let
 
 in stdenv.mkDerivation (cleanedArgs // {
 
-  # TODO: should we do the change in postPatch instead?
   postPatch = ''
-    echo ${downloadedDeps}
     for f in `find . -name "*.roc" -type f`; do
-        echo "$f"
-        # substituteInPlace $f
+        ${jq}/bin/jq -r '[.url, .tar] | @tsv' ${downloadedDeps}/roc-packages.json |
+          while IFS=$'\t' read -r url tar; do
+            substituteInPlace $f \
+              --replace "$url" "$tar"
+            cat $f
+          done
     done
   '';
 
